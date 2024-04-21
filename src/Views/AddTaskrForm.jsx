a3a20aff3193b75e223f05c03aa19bba7949811f
wwIrvingww <98479343+wwIrvingww@users.noreply.github.com>
2024-04-21 09:12:20 -0600

    comment

--- src/Views/AddTaskrForm.jsx
+++ src/Views/AddTaskrForm.jsx
@@ -3,20 +3,35 @@ import PropTypes from 'prop-types';
 
 import './AddTaskForm.css';
 
+/**
+ * A form component for adding new tasks.
+ * Handles input field state and task submission.
+ * @param {function} onAddTask - Callback function to add a new task.
+ */
 const AddTaskForm = ({ onAddTask }) => {
+  // Initialize state for task title and description
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
 
+  /**
+   * Handles form submission and task creation.
+   * @param {object} e - Submit event object.
+   */
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!e.target.checkValidity()) return;
 
+    // Generate a unique ID for the new task
     const newTask = {
       id: Math.floor(Math.random() * 1000),
-      title: title.trim(),
-      description: description.trim(),
+      title: title.trim(), // Remove leading/trailing whitespace from title
+      description: description.trim(), // Remove leading/trailing whitespace from description
     };
+
+    // Add the new task using the provided callback function
     onAddTask(newTask);
+
+    // Reset form and input fields
     e.target.reset();
     setTitle('');
     setDescription('');
@@ -24,30 +39,26 @@ const AddTaskForm = ({ onAddTask }) => {
 
   return (
     <div className='form-group'>
-      <form onSubmit={handleSubmit} >
-        <input className='input'
+      <form onSubmit={handleSubmit}>
+        {/* Title input field */}
+        <input
+          className='input'
           type="text"
           placeholder="Título de la tarea"
           required
-          minLength={3}
+          minLength={3} // Minimum length validation
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
-        
-        <textarea className='textarea'
+
+        {/* Description textarea field */}
+        <textarea
+          className='textarea'
           placeholder="Descripción de la tarea"
           required
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         ></textarea>
-        <button className='button'type="submit">Agregar Tarea</button>
-      </form>
-    </div>
-  );
-};
 
-AddTaskForm.propTypes = {
-  onAddTask: PropTypes.func.isRequired,
-};
-
-export default AddTaskForm;
+        {/* Submit button */}
+        <button className='button' type="submit">Agreg
