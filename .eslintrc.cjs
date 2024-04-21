15914f2570d1e416996d09f5637934a876faf3fb
wwIrvingww <98479343+wwIrvingww@users.noreply.github.com>
2024-04-21 09:05:26 -0600

    improve

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,13 +9,11 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: {
+    react: {
+      version: '18.2',
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+
