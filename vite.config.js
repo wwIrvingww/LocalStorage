8dc3ab8381f49b801793cf0c351607fa331d5b3c
wwIrvingww <98479343+wwIrvingww@users.noreply.github.com>
2024-04-21 09:12:17 -0600

    comment

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // Define a type for the Vite configuration
 interface IViteConfig {
-  plugins: react.Plugin[]
+  plugins: react.Plugin[] // Specify that the plugins property is an array of react.Plugin objects
 }
 
 // Export a default configuration object
-export default defineConfig({
+export default defineConfig({ // The defineConfig function is used to configure Vite
   // Give the configuration a more descriptive name
   configName: 'MyAppConfig',
-  // Cast the configuration object to the IViteConfig type
+  // Cast the configuration object to the IViteConfig type to ensure type safety
 } as IViteConfig): IViteConfig {
-  plugins: [react()],
+  plugins: [react()], // Include the react plugin to enable support for React in the project
 }
